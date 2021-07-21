d3f8cfca83e4de6d28152b9b7bb775e96e13b58f
awesomemoder316 <thesusefulthing@gmail.con>
2021-07-21 14:14:45 -0700

    Automatically convert legacy settings.

--- src/main/java/me/awesomemoder316/passwordlogon/PasswordLogOn.java
+++ src/main/java/me/awesomemoder316/passwordlogon/PasswordLogOn.java
@@ -7,7 +7,12 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
+import java.util.Scanner;
 
 
 public class PasswordLogOn extends JavaPlugin {
@@ -36,6 +41,35 @@ public class PasswordLogOn extends JavaPlugin {
         new Metrics(this, 11006);
 
         Utils.check();
+
+        //Transfer legacy settings. Check file manually instead of config cause getDouble() returns 0 if null.
+        List<String> oldLocSettings = Arrays.asList("x1", "x2", "y", "z1", "z2");
+        try {
+
+            for (String setting: oldLocSettings) {
+
+                File file = new File(getDataFolder() + File.separator + "config.yml");
+                Scanner scanner = new Scanner(file);
+
+                while (scanner.hasNextLine()) {
+
+                    String line = scanner.nextLine();
+                    if (line.startsWith(setting)) {
+                        double value = Double.parseDouble(line.replace(setting + ": ", ""));
+                        getConfig().set("overworld." + setting, value);
+                        getConfig().set("nether." + setting, value);
+                        getConfig().set("end." + setting, value);
+                        getConfig().set(setting, null);
+                        getConfig().options().copyDefaults(true);
+                        saveConfig();
+                    }
+                }
+
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     public void onDisable() {
