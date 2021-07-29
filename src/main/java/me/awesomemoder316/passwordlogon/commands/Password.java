93b723641098b4498016a98f8647954272c1e3d6
awesomemoder316 <thesusefulthing@gmail.con>
2021-07-29 10:45:35 -0700

    Fix setArea; added setArea tabcomplete; fixed wrong message sent.

--- src/main/java/me/awesomemoder316/passwordlogon/commands/Password.java
+++ src/main/java/me/awesomemoder316/passwordlogon/commands/Password.java
@@ -21,7 +21,7 @@ public class Password implements CommandExecutor, TabCompleter {
 
                 if (args.length < 2) {
                     new MessageConfig().incorrectUsage(sender, "generic");
-                    return false;
+                    return true;
                 }
 
                 switch (args[0].toLowerCase()) {
@@ -29,14 +29,14 @@ public class Password implements CommandExecutor, TabCompleter {
 
                         if (!(sender instanceof Player)) {
                             new MessageConfig().rejectConsole(sender);
-                            return false;
+                            return true;
                         }
 
                         Player p = (Player) sender;
 
                         if (Utils.plugin.getConfig().contains("password." + p.getUniqueId())) {
                             new MessageConfig().alreadySetPassword(sender);
-                            return false;
+                            return true;
                         }
 
                             String playerPassword = args[1];
@@ -51,24 +51,25 @@ public class Password implements CommandExecutor, TabCompleter {
 
                         if (!(sender instanceof Player)) {
                             new MessageConfig().rejectConsole(sender);
-                            return false;
+                            return true;
                         }
 
                         p = (Player) sender;
 
                         if (!(Utils.plugin.getConfig().contains("password." + p.getUniqueId()))) {
                             new MessageConfig().noPasswordSet(sender);
-                            return false;
+                            return true;
                         }
                             if (args.length < 3) {
                                 new MessageConfig().incorrectUsage(sender, "reset");
-                                return false;
+                                return true;
                             }
                                 String s = Utils.plugin.getConfig().getString("password." + p.getUniqueId());
                                 String oldPassword = args[1];
                                 String newPassword = args[2];
 
-                                if (s.equals(oldPassword)) {
+                        assert s != null;
+                        if (s.equals(oldPassword)) {
                                     Utils.plugin.getConfig().set("password." + p.getUniqueId(), newPassword);
                                     Utils.plugin.getConfig().options().copyHeader(true);
                                     Utils.plugin.saveConfig();
@@ -81,12 +82,12 @@ public class Password implements CommandExecutor, TabCompleter {
 
                     case "setarea":
 
-                        if (!(sender instanceof Player)) {
+                        if (sender instanceof Player) {
                             new MessageConfig().rejectPlayer(sender);
                         }
 
                         if (args.length < 7) {
-                            new MessageConfig().passwordSetConfirmation(sender, "setArea");
+                            new MessageConfig().incorrectUsage(sender, "setArea");
                             return true;
                         }
 
@@ -100,8 +101,8 @@ public class Password implements CommandExecutor, TabCompleter {
                                 break;
 
                             default:
-                                new MessageConfig().passwordSetConfirmation(sender, "setArea");
-                                return false;
+                                new MessageConfig().incorrectUsage(sender, "setArea");
+                                return true;
                         }
 
                         if (cannotParseDouble(args[2]) ||
@@ -110,14 +111,14 @@ public class Password implements CommandExecutor, TabCompleter {
                                 cannotParseDouble(args[5]) ||
                                 cannotParseDouble(args[6])
                         ) {
-                            new MessageConfig().passwordSetConfirmation(sender, "setAreaParameterType");
+                            new MessageConfig().incorrectUsage(sender, "setAreaParameterType");
                             return true;
                         }
-                        Utils.plugin.getConfig().set(dimension + ".x1", Double.parseDouble(args[1]));
-                        Utils.plugin.getConfig().set(dimension + ".x2", Double.parseDouble(args[2]));
-                        Utils.plugin.getConfig().set(dimension + ".y", Double.parseDouble(args[3]));
-                        Utils.plugin.getConfig().set(dimension +".z1", Double.parseDouble(args[4]));
-                        Utils.plugin.getConfig().set(dimension + ".z2", Double.parseDouble(args[5]));
+                        Utils.plugin.getConfig().set(dimension + ".x1", Double.parseDouble(args[2]));
+                        Utils.plugin.getConfig().set(dimension + ".x2", Double.parseDouble(args[3]));
+                        Utils.plugin.getConfig().set(dimension + ".y", Double.parseDouble(args[4]));
+                        Utils.plugin.getConfig().set(dimension +".z1", Double.parseDouble(args[5]));
+                        Utils.plugin.getConfig().set(dimension + ".z2", Double.parseDouble(args[6]));
                         Utils.plugin.getConfig().options().copyHeader(true);
                         Utils.plugin.saveConfig();
                 }
@@ -128,25 +129,67 @@ public class Password implements CommandExecutor, TabCompleter {
 
     public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
 
-        if (args.length > 3) {
-            return StringUtil.copyPartialMatches(args[3], Collections.singletonList(""), new ArrayList<>());
+        if (args.length == 0) return null;
 
-        } else if (args.length > 2 && args[0].equalsIgnoreCase("reset")) {
+        if (args.length == 1)
+            return StringUtil.copyPartialMatches(args[0], Arrays.asList("reset", "set", "setarea"), new ArrayList<>());
 
-            return StringUtil.copyPartialMatches(args[2], Collections.singletonList("(new password)"), new ArrayList<>());
 
-        } else if (args.length > 1) {
+        switch (args[0].toLowerCase()) {
 
-            if (args[0].equalsIgnoreCase("reset")) {
+            case "reset":
 
-                return StringUtil.copyPartialMatches(args[1], Collections.singletonList("(old password)"), new ArrayList<>());
+                switch (args.length) {
+                    //Not 1 as alr checked.
 
-            } else if (args[0].equalsIgnoreCase("set")) {
+                    case 2:
+                        return StringUtil.copyPartialMatches(args[1], Collections.singletonList("(old password)"), new ArrayList<>());
 
-                return StringUtil.copyPartialMatches(args[1], Collections.singletonList("(password)"), new ArrayList<>());
-            }
+                    case 3:
+                        return StringUtil.copyPartialMatches(args[2], Collections.singletonList("(new password)"), new ArrayList<>());
+
+                    default:
+                        return StringUtil.copyPartialMatches(args[3], Collections.singletonList(""), new ArrayList<>());
+                }
+
+            case "set":
+
+                if (args.length == 2) {
+                    return StringUtil.copyPartialMatches(args[1], Collections.singletonList("(password)"), new ArrayList<>());
+                }
+                return StringUtil.copyPartialMatches(args[2], Collections.singletonList(""), new ArrayList<>());
+
+            case "setarea":
+
+                switch (args.length) {
+
+                    case 2:
+                        return StringUtil.copyPartialMatches(args[1], Arrays.asList("end", "nether", "overworld"), new ArrayList<>());
+
+                    case 3:
+                        return StringUtil.copyPartialMatches(args[2], Collections.singletonList("(x1)"), new ArrayList<>());
+
+                    case 4:
+                        return StringUtil.copyPartialMatches(args[3], Collections.singletonList("(x2)"), new ArrayList<>());
+
+                    case 5:
+                        return StringUtil.copyPartialMatches(args[4], Collections.singletonList("(y)"), new ArrayList<>());
+
+                    case 6:
+                        return StringUtil.copyPartialMatches(args[5], Collections.singletonList("(z1)"), new ArrayList<>());
+
+                    case 7:
+                        return StringUtil.copyPartialMatches(args[6], Collections.singletonList("(z2)"), new ArrayList<>());
+
+                    default:
+                        return StringUtil.copyPartialMatches(args[7], Collections.singletonList(""), new ArrayList<>());
+
+                }
+
+            default:
+                return StringUtil.copyPartialMatches(args[1], Collections.singletonList(""), new ArrayList<>());
+
         }
-        return (args.length == 1) ? StringUtil.copyPartialMatches(args[0], Arrays.asList("reset", "set"), new ArrayList<>()) : null;
     }
 
     private boolean cannotParseDouble(String arg) {
