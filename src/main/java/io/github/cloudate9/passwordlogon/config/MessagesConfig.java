6d0508a75aa01828107ecc10058d18a8bf8042fe
Cloudate9 <65006925+Cloudate9@users.noreply.github.com>
2022-07-14 10:37:56 -0700

    Fix duplicate rejection message for setarea

---
+++ src/main/java/io/github/cloudate9/passwordlogon/config/MessagesConfig.java
@@ -0,0 +1,28 @@
+package io.github.cloudate9.passwordlogon.config;
+/*
+import com.github.secretx33.sccfg.api.annotation.Configuration;
+import com.github.secretx33.sccfg.api.annotation.Path;
+
+@Configuration(
+        value = "messages.yml",
+        header = "PasswordLogOn uses MiniMessage (https://docs.adventure.kyori.net/minimessage) for messages."
+)*/
+public class MessagesConfig {
+/*
+    @Path("v2.error")
+    public String incorrectPassword = "<red>You entered an incorrect password!</red>";
+
+    @Path("v2.error")
+    public String passwordExists = "<red>You already have a password set!</red>";
+
+    @Path("v2.command")
+    public String passwordSetHelp = "<gold>/pw set <password></gold>";
+
+    @Path("v2.command")
+    public String passwordResetHelp = "<gold>/pw reset <old password> <new password></gold>";
+
+    @Path("v2.command")
+    public String setAreaHelp = "<gold>/pw setarea <x1> <x2> <y> <z1> <z2>: Set the new login platform in a </gold>";
+
+*/
+}
